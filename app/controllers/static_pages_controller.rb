0aa8fbc0a4c30406978bffaf19c7e0e1ea1011fc
Perry Zheng <perry.zheng@gmail.com>
2013-02-27 23:33:47 -0800

    add diana working at meesum

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,4 +14,38 @@ class StaticPagesController < ApplicationController
   def projects
     
   end
+
+  def diana
+    time = Time.now
+    if time.wday == 6
+      working = TimeRange.new("18:00","21:00").include?(time)    
+    elsif time.wday == 2
+      working = TimeRange.new("17:30","22:00").include?(time)
+    elsif time.wday == 4
+      working = TimeRange.new("12:30","17:00").include?(time)
+    end
+    @isworking = working ? "Yes!" : "No" 
+  end
+end
+
+class TimeRange
+    private
+
+    def coerce(time)
+        time.is_a? String and return time
+        return time.strftime("%H:%M")
+    end
+
+    public
+
+    def initialize(start,finish)
+        @start = coerce(start)
+        @finish = coerce(finish)
+    end
+
+    def include?(time)
+        time = coerce(time)
+        @start < @finish and return (@start..@finish).include?(time)
+        return !(@finish..@start).include?(time)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/about', to: 'static_pages#about'
+  match '/isdianaworkingatmeesumnow', to:'static_pages#diana'
   match '/contact', to: 'static_pages#contact'
   match '/projects', to: 'static_pages#projects'
 end
